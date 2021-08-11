1d71bdb837c541c7f8714de0afec071af3e3d60e
unknown <rakshithbabu111@gmail.com>
2021-08-11 23:11:14 +0530

    basic

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const Discord = require('discord.js');
-const client = new Discord.client();
-console.log("asd")
+const { token } = require('./config.json');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.login(token);
