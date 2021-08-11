28c6bde2de111f0986b75116e62225131cb11c4c
unknown <rakshithbabu111@gmail.com>
2021-08-11 21:43:23 +0530

    asd

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 const Discord = require('discord.js');
 const client = new Discord.client();
 console.log("asd")
-console.log("asd")
-console.log("asd")
-console.log("asd")
-
-//git push --set-upstream origin mastergit commit -m "asd"
-//git push --set-upstream origin master
