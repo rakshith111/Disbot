5d32b94bda18bee5216439c7ff78098273fbf1e7
unknown <rakshithbabu111@gmail.com>
2021-08-12 18:03:28 +0530

    new listener

--- main.js
+++ main.js
@@ -17,36 +17,60 @@ const voiceCollection = new Collection();
 //         console.log(args);
 //     }   
 // })
-
-
-
 var count = 0
 var name = "Voice #"
+// client.on('voiceStateUpdate',async(oldstate,newstate)=>{
+//     const user = await client.users.fetch(newstate.id);
+//     const member = newstate.guild.member(user);
 
-
-client.on('voiceStateUpdate',async(oldstate,newstate)=>{
-    const user = await client.users.fetch(newstate.id);
-    const member = newstate.guild.member(user);
-
-    if(!oldstate.channel && newstate.channel.id === '875331035112566804'){ //voice channel id  ez gotta make a list like this      
-        count = count +1
-        console.log(name+count)
+//     if(!oldstate.channel && newstate.channel.id === '875331035112566804'){ //voice channel id  ez gotta make a list for this       
+//         count = count +1
+//         console.log(name+count)
         
-        const channel = await newstate.guild.channels.create(name+count,{
-            type:'voice',
-            parent :newstate.channel.parent,
-        });     
-        member.voice.setChannel(channel);
-        voiceCollection.set(user.id,channel.id); //  creates a map uid : channel id
-        console.log(voiceCollection)
+//         const channel = await newstate.guild.channels.create(name+count,{
+//             type:'voice',
+//             parent :newstate.channel.parent,
+//         });     
+//         member.voice.setChannel(channel);
+//         console.log(channel.id)
+//         voiceCollection.set(channel.id,user.id); //  creates a map channel id :uid 
+//         console.log(voiceCollection)
+//     }
+//      //else if (!newstate.channel ){
+//     //     flag=0
+//     //     if(oldstate.channelID===voiceCollection.get(newstate.id) ) 
+//     //         count=count-1;
+//     //         console.log("delete count " +count)
+//     //         return oldstate.channel.delete();  //
+//     // }
+// });
+const mainChannel = '875331035112566804';
+const mainCategory = '875292443917037610';
+const temporaryChannels = new Set();
+client.on('voiceStateUpdate', async (oldVoiceState, newVoiceState) => {
+    try {
+        const {channelID: oldChannelId, channel: oldChannel} = oldVoiceState;
+        const {channelID: newChannelId, guild, member} = newVoiceState;
+        if (newChannelId === mainChannel) {
+
+            const channel = await guild.channels.create(
+                `${member.user.username}'s channel`,
+                {type: 'voice', parent: mainCategory}
+            );
+            temporaryChannels.add(channel.id);
+            await newVoiceState.setChannel(channel);
+        }
+        if (
+            temporaryChannels.has(oldChannelId) &&
+            oldChannelId !== newChannelId
+        ) {
+            await oldChannel.delete();
+            temporaryChannels.delete(oldChannelId);
+        }
+    } catch (error) {
+        // Handle any errors
+        console.error(error);
     }
-     else if (!newstate.channel ){
-    //     flag=0
-    //     if(oldstate.channelID===voiceCollection.get(newstate.id) ) 
-    //         count=count-1;
-    //         console.log("delete count " +count)
-    //         return oldstate.channel.delete();  //
-    // }
 });
 
 
