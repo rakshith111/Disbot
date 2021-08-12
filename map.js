eaacb6700a9ffed0615903c50c727cad48a953da
unknown <rakshithbabu111@gmail.com>
2021-08-12 18:04:27 +0530

    new listener

---
+++ map.js
@@ -0,0 +1,7 @@
+// var readline = require("readline");
+// var rl = readline.createInterface(process.stdin, process.stdout);
+var obj = {}
+obj.key1=new Array();
+obj.key2=new Array();
+obj.key2.push("asd")
+console.log(obj)
