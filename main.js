4c51c62ecf27ceb8976f63de7e53dcf8a8d14553
unknown <rakshithbabu111@gmail.com>
2021-08-12 16:29:19 +0530

    add func added

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');	
 const { token } = require('./config.json');
+prefix="!";
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })    
 client.once('ready', () => {
 	console.log('Ready!');
 });
 const{Collection}=require('discord.js');
 const voiceCollection = new Collection();
-prefix="!";
+
+// for user to add new ids 
 // client.on('message',async msg=>{
 //     if(msg.author.bot) return
 //     const args = msg.content.slice(prefix.length).split(/ +/);
@@ -15,25 +17,38 @@ prefix="!";
 //         console.log(args);
 //     }   
 // })
-// general  vc id 875254954342891540
-client.on('voiceStateUpdate',async(oldstate,newstate)=>{
 
+
+
+var count = 0
+var name = "Voice #"
+var flag=0
+
+client.on('voiceStateUpdate',async(oldstate,newstate)=>{
     const user = await client.users.fetch(newstate.id);
     const member = newstate.guild.member(user);
-    if(user.bot) console.log("aaaaaa")
-    if(!oldstate.channel && newstate.channel.id === '875289644365914132'){
-        const channel = await newstate.guild.channels.create(user.tag,{
+
+    if(!oldstate.channel && newstate.channel.id === '875331035112566804'){ //voice channel id  ez gotta make a list like this      
+        flag=1
+        count = count +1
+        console.log(name+count)
+        
+        const channel = await newstate.guild.channels.create(name+count,{
             type:'voice',
             parent :newstate.channel.parent,
-        });
+        });     
         member.voice.setChannel(channel);
-        voiceCollection.set(user.id,channel.id);
-
-    }else if (!newstate.channel){
-        if(oldstate.channelID===voiceCollection.get(newstate.id)) 
-            return oldstate.channel.delete();
+        voiceCollection.set(user.id,channel.id); // map a collection to add users who are moved  proly have to change this method
+        console.log(voiceCollection)
     }
+    // else if (!newstate.channel && flag === 1){
+    //     flag=0
+    //     if(oldstate.channelID===voiceCollection.get(newstate.id) ) 
+    //         count=count-1;
+    //         console.log("delete count " +count)
+    //         return oldstate.channel.delete();  //
+    // }
 });
 
 
-client.login(token);
+ client.login(token);
