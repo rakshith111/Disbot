0475a4d44300cb7241854e6cc5e084bb472c6c1f
unknown <rakshithbabu111@gmail.com>
2021-08-17 08:00:16 +0530

    added a reader

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
 const Discord = require('discord.js');	
+const { orderBy } = require('natural-orderby')
 const { token } = require('./config.json');
-prefix="!";
 const client = new Discord.Client()    
 client.once('ready', () => {
 	console.log('Ready!');
 });
-const{Collection}=require('discord.js');
-const voiceCollection = new Collection();
-
 var dict = {};
+var fs = require('fs');
+const prefix="!";
+
+const CsvReadableStream = require('csv-reader');
 client.on('message',async msg=>{
     if(msg.author.bot) return
+    if(msg.content === '!set help'){
+         client.channels.cache.get(msg.channel.id).send("Type \n !set <vc's catagory id>  <main vc's id>")
+          return
+    }
     const args = msg.content.slice(prefix.length).split(/ +/);
     if(msg.content.startsWith(prefix))
+    console.log(args.length)
     switch(args[0]){
-        case 'set': dict[`${args[1]}`] =args[2];
-                    console.log(dict)
+        case 'set': if(args.length === 3){
+                        console.log("worik")
+                            if(args[1].length  && args[2].length === 18 ){
+                                dict[`${args[1]}`] =args[2];
+                                fs.readFile('./head.json', 'utf8', (err, fileContents) => {
+                                    if (err) {
+                                      console.error(err)
+                                      return
+                                    }
+                                    try {   
+                                    var data = JSON.parse(fileContents) // this data contains both the ids till here $use this to count and create new instances
+                                    dat=Object.assign(data,dict)
+                                    console.log(dat)
+                                    fs.writeFile("head.json", JSON.stringify(dat), err => {
+                                        if (err) throw err; 
+                                        console.log(" Written"); // Success
+                                    });
+                                    } catch(err) {
+                                      console.error(err)
+                                    
+                                    }
+                                  })
+                            }
+                            else{
+                                client.channels.cache.get(msg.channel.id).send("Enter the correct id's")
+
+                            }
+                    }
+                    else{
+                        client.channels.cache.get(msg.channel.id).send("Enter the correct id's")
+                    }
+
                     break;
     }
-});
-
+})
 var count = 0
 const mainCategory = '875292443917037610';
 const mainChannel = '875331035112566804';
@@ -27,14 +62,13 @@ const temporaryChannels = new Set();
 var c = new Boolean(false);
 //generates a name storage array 
 function gen(nam){
-    var limit =1 ;
     names = new Array();
-    names[0]=nam;
-    for(let i=0; i<limit+1 ; i++){
+    names[0]=nam;               
+    for(let i=0; i<20 ; i++){
         names[1+i]=nam+" " +(1+i);
 
     }
-    //console.log(names)
+    names.sort();
     c=false;
 }
 var temp_main=mainChannel;;
@@ -44,22 +78,28 @@ client.on('voiceStateUpdate', async (oldVoiceState, newVoiceState) => {
         const {channelID: newChannelId, guild, member} = newVoiceState;
         var nam = client.channels.cache.get(mainChannel)
         if (c) gen(nam.name); // generates a name arr once
-
-        if (newChannelId === temp_main) {
+        names=orderBy(names)
+        console.log(names)
+        
+        
+        if (newChannelId === temp_main && names.length >=1  ) {
+             
             const channel = await guild.channels.create(
                 names[1],
                 {type: 'voice', parent: mainCategory}
             );
             temporaryChannels.add(channel.id);
             temp_main=channel.id;
-            names.splice(names.indexOf(names[1]),1)            
+            names.splice(names.indexOf(names[1]),1)   
+            names=orderBy(names)
+         
             //await newVoiceState.setChannel(channel); //moves user to new channel
         }
         // Remove empty temporary channels
         if ( temporaryChannels.has(oldChannelId) && oldChannelId !== newChannelId && oldChannel.members.size === 0 ) {
             var n = client.channels.cache.get(oldChannelId);
             names.push(n.name)
-            names.sort();
+            names=orderBy(names)
             await oldChannel.delete();
             temporaryChannels.delete(oldChannelId);
         }
