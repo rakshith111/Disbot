73907ae45b9045419d0b24a335daaa2e973db94d
rakshith111 <36219488+rakshith111@users.noreply.github.com>
2021-08-17 07:59:23 +0530

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import re
-price_dict = {'#buyCursor': 'Cursor - 15', '#buyGrandma': 'Grandma - 100', '#buyFactory': 'Factory - 500', '#buyMine': 'Mine - 2000', 'buyShipment': 'Shipment - 7000', '#buyAlchemy lab': 'Alchemy lab - 50000', 'buyPortal': 'Portal - 1000000', '#buyTime machine': 'Time machine - 123456789'}
-final_price = {key:int( "".join((re.findall(r'\d+', value)))) for (key, value) in price_dict.items() }
-print(final_price)
-print(price_dict)
